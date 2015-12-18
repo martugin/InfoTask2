b82ca306360f40e53a32d3d1672edbbbf309cd70
Martyugin P <martuginp@gmail.com>
2015-12-18 09:51:23 +0500

    Коммит

--- MainSources/Siemens/SignalSimaic.cs
+++ MainSources/Siemens/SignalSimaic.cs
@@ -9,7 +9,7 @@ namespace Provider
         public SignalSimatic(string signalInf, string code, DataType dataType, IProvider provider, int idInClone = 0) 
             : base(signalInf, code, dataType, provider, idInClone)
         {
-            Id = Inf.GetInt("Id");
+            Id = Inf.GetInt("Id"); 
         }
 
         //Id в таблице архива
