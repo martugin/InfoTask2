97bb39b9a8059691a0cded3b6e5ede97fb7c12cf
Martyugin P <mpv@ec-ute.ru>
2017-04-04 18:00:16 +0500

    RealTimeServer

---
+++ MainSources/RealTimeServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("RealTimeServer")]
+[assembly: AssemblyDescription("Сервер получения данных в режиме реального времени системы InfoTask")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("InfoTask")]
+[assembly: AssemblyProduct("RealTimeServer")]
+[assembly: AssemblyCopyright("Copyright © InfoTask 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e9417c69-39bf-4e87-9fe9-098f88baea96")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
---
+++ MainSources/RealTimeServer/Threads/RealTimeSourceThread.cs
@@ -0,0 +1,25 @@
+﻿using ProvidersLibrary;
+
+namespace RealTimeServer
+{
+    //Поток получения архивных данных из истоника в режиме реального времени
+    public class RealTimeSourceThread : RealTimeThread
+    {
+        public RealTimeSourceThread(MainClass main,
+                                                     string appCode, //Код приложения
+                                                     string project, //Код проекта
+                                                     string connCode, //Код соединения
+                                                     string complect) //Комплект соединения
+            : base(main, appCode, project)
+        {
+            Connect = (SourceConnect)MainClass.ProvidersFactory.CreateConnect(ProviderType.Source, connCode, complect, this);
+        }
+
+        //Соединение с источником
+        public SourceConnect Connect { get; private set; }
+
+        //Сдвиг в секундах времени начала и конца периода считывания данных от текущего времени
+        public int ReadingShiftBegin { get; private set; }
+        public int ReadingShiftEnd { get; private set; }
+    }
+}
