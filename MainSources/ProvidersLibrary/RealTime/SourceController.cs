a3a197e47945f38adf86b0281e5aa49c4b74bc4b
Martyugin P <mpv@ec-ute.ru>
2017-04-14 09:01:14 +0500

    Провайдеры. Тесты

---
+++ MainSources/ProvidersLibrary/RealTime/SourceController.cs
@@ -0,0 +1,27 @@
+﻿
+namespace ProvidersLibrary
+{
+    //Постоянное получение данных с источника в реальном времени
+    public class SourceController
+    {
+        public SourceController(SourceConnect connect)
+        {
+            Connect = connect;
+        }
+
+        //Соединение 
+        public SourceConnect Connect { get; private set; }
+            
+        //Запуск процесса считывания
+        public void Start()
+        {
+            
+        }
+
+        //Завершение процесса считывания
+        public void Finish()
+        {
+            
+        }
+    }
+}
